bdca4331b443af287ee80bd90654b085bd360f48
Sorav <codesorcerers@gmail.com>
2022-06-29 22:25:02 +0530

    project backend setup

---
+++ server.js
@@ -0,0 +1,8 @@
+const app = require("./app");
+const dotenv = require("dotenv");
+//config
+dotenv.config({path:'./config/config.env'});
+
+app.listen(process.env.PORT,()=>{
+    console.log("server running on "+ process.env.PORT);
+})
